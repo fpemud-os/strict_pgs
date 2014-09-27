007ae05e4ad4551143e0a9146225b64ae0bbc924
fpemud <fpemud@sina.com>
2014-09-27 20:48:08 +0800

    fix

--- scripts/git-hook-pre-commit.sh
+++ scripts/git-hook-pre-commit.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-OUTPUT=`pep8 python3/strict_pgs.py | grep -v "E501"`
-if [ -n "$OUTPUT" ]; then
-    echo $OUTPUT
-    exit 1
+FILES="python3/strict_pgs.py"
+ERRFLAG=0
+
+OUTPUT1=`pyflakes "${FILES}"`
+OUTPUT2=`pep8 "${FILES}" | grep -v "E501"`
+
+if [ -n "$OUTPUT1" ] ; then
+    echo "pyflake errors:"
+    echo "$OUTPUT1"
+    ERRFLAG=1
+fi
+
+if [ -n "$OUTPUT2" ] ; then
+    echo "pep8 errors:"
+    echo "$OUTPUT2"
+    ERRFLAG=1
+fi
+
+if [ "${ERRFLAG}" == 1 ] ; then
+    exit
 fi
